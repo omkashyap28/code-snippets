32ef42bd418f81927fd28747997d1d862d9b8ad7
Hariom Kashyap <omkashyap6870@gmail.com>
2026-02-03 22:26:40 +0530

    database query

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as codeExecution from "../codeExecution.js";
 import type * as http from "../http.js";
 import type * as users from "../users.js";
 
@@ -18,6 +19,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  codeExecution: typeof codeExecution;
   http: typeof http;
   users: typeof users;
 }>;
