2b707329cc147a6bfb3cc375fb36bca2504f2428
Hariom Kashyap <omkashyap6870@gmail.com>
2026-02-03 22:27:41 +0530

    root header

---
+++ src/app/(root)/_components/header-profile-btn.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import LoginButton from "@/components/login-btn";
+import { SignedOut, UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
+
+export default function HeaderProfileBtn() {
+  return (
+    <>
+      <UserButton>
+        <UserButton.MenuItems>
+          <UserButton.Link
+            label="Profile"
+            labelIcon={<User className="size-4" />}
+            href="/profile"
+          />
+        </UserButton.MenuItems>
+      </UserButton>
+
+      <SignedOut>
+        <LoginButton />
+      </SignedOut>
+    </>
+  );
+}
